f68a755d63359420224d9df426c22fc3f69b5cfa
lucapolzone <lucapolzone@gmail.com>
2024-02-15 00:00:41 +0100

    feat: first layout & test global state

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+  import { store } from '../store/';
+  
+  export default {
+    data() {
+      return {
+        store
+      }
+    }
+  };
+</script>
+
+<template>
+  <header>
+    <h1> {{ store.title }} </h1>
+    <form>
+      <input type="search">
+      <input type="submit" value="submit">
+    </form>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+  title: 'Titolo di prova' 
+})
